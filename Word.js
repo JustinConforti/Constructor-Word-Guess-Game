269d30943f70ad2707ace4b9224355f6b6580b54
Justin <justinconforti@gmail.com>
2019-07-02 16:21:19 -0400

    created Word constructor and objArray

--- Word.js
+++ Word.js
@@ -1,2 +1,15 @@
 var Letters = require("./letter.js");
 
+function Word(answer) {
+        
+        // object array that stores each character letter or underscore if not guessed yet
+    this.objArray = [];
+
+        // loop that will take each index of the answer parameter run it through the Letters constructor functions and push to objArray array.
+    for (let i = 0; i < answer.length; i++) {
+        let letter = new Letters(answer[i]);
+        this.objArray.push(letter)
+    }
+
+    
+}
